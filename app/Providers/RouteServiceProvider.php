416081cd95acbb10648ed9e2481a5bbfa07cdcfc
reylyer <reylyercross@gmail.com>
2024-02-03 09:40:37 +0700

    sync

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        \Illuminate\Support\Facades\URL::forceRootUrl(\Illuminate\Support\Facades\Config::get('app.url'));
+
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
